e16f83b2cb5b3279785c886cf903040e8720803d
Walker Hale IV <walker.hale.iv@gmail.com>
2017-02-20 10:40:46 -0600

    Working on getting the daemon running

--- seneschald.py
+++ seneschald.py
@@ -7,16 +7,20 @@ processing or export of data located within the protected area."""
 import argparse
 import logging
 import logging.config
+import sys
+import time
 
 import foo
 
 import daemon
+import lockfile
 import yaml
 
 from seneschal import *
 
 
 logger = logging.getLogger('seneschald')
+running = True  # When False, will exit polling loop.
 
 
 def main():
@@ -24,8 +28,14 @@ def main():
     config = load_config_file(args.config_file)
     config_logging(config.pop('logging', None))
     logger.debug('args: %r', vars(args))
+    daemon_config = config.pop('daemon')
+    seneschal_config = config.pop('seneschal')
+    daemon_command = args.daemon_command
     try:
-        run(config)
+        if daemon_command == 'start':
+            start(daemon_command, daemon_config, seneschal_config)
+        elif daemon_config == 'stop':
+            pass  # TODO
     finally:
         logging.shutdown()
 
@@ -54,9 +64,49 @@ def config_logging(logging_config_dict):
         logging.basicConfig(level=logging.NOTSET)
 
 
-def run(config):
-    logger.debug('config: %r', config)
+def start(daemon_command, daemon_config, seneschal_config):
+    logger.debug('daemon_config: %r', daemon_config)
+    logger.debug('seneschal_config: %r', seneschal_config)
     logger.debug('========================================')
+    check_for_illegal_daemon_options(daemon_config)
+    daemon_kwds = {k: v for k, v in daemon_config.items() if v is not None}
+    pidfile_path = daemon_kwds.pop('pidfile')
+    pidfile = lockfile.FileLock(pidfile_path)
+    # TODO: check for already running process
+    logger.debug('daemon_kwds: %r', daemon_kwds)
+    # The remaining entries in daemon_kwds will be passed as-is to
+    # daemon.DaemonContext.
+    context = daemon.DaemonContext(pidfile=pidfile, **daemon_kwds)
+    with context:
+        while running:
+            logger.info('ping')
+            time.sleep(0.5)
+            # TODO: Long polling times, may result in an unacceptable
+            # delay during daemon shutdown.
+
+
+def check_for_illegal_daemon_options(daemon_config):
+    """Error out and die if any illegal options."""
+    LEGAL_DAEMON_OPTIONS = set('''
+        pidfile
+        working_directory
+        chroot_directory
+        umask
+        detach_process
+        uid
+        gid
+        prevent_core
+    '''.split())
+    illegal_options = set(daemon_config) - LEGAL_DAEMON_OPTIONS
+    if illegal_options:
+        logger.critical('illegal daemon options in YAML config file: %r',
+                        list(illegal_options))
+        sys.exit(1)
+
+
+def trigger_shutdown(signum, frame):
+    """Set global running to False, to trigger shutdown."""
+    running = False
 
 
 if __name__ == "__main__":
